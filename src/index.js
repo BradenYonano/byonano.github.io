eb5f7aaf2c88927d5c415e1c16af0ca90054af81
Braden Yonano <byonano@gmail.com>
2022-04-23 00:16:17 -0700

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Minesweeper from "./App"
 import reportWebVitals from './reportWebVitals';
+
+/*
+var element = React.createElement("h1", {className: "greeting"}, "text");
+ReactDOM.render(element, document.getElementById("root"));
+
+var b1 = React.createElement("button", {className: "bc1"}, "Basic Button");
+ReactDOM.render(b1, document.getElementById("root"));
+*/
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Minesweeper/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
 reportWebVitals();
